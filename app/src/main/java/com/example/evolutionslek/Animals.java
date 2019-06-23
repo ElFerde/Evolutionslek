ee09b2e65cba4d1278aa00306ad1807d56c19453
Ruth <risberg.ruth@gmail.com>
2019-06-23 11:24:28 +0200

    added three species

--- app/src/main/java/com/example/evolutionslek/Animals.java
+++ app/src/main/java/com/example/evolutionslek/Animals.java
@@ -12,9 +12,42 @@ public class Animals {
     boolean herbivore;
 }
 
+class Husmus extends Animals {
+    public Husmus() {
+        mass = 5;
+        defense = 1;
+        maxHealth = mass;
+        health = maxHealth;
+        horns = 1;
+        attack = (int) Math.round(Math.sqrt(mass)*horns);
+        herbivore = true;
+    }
+}
+class Unicorn extends Animals {
+    public Unicorn() {
+        mass = 80;
+        defense = 4;
+        maxHealth = mass;
+        health = maxHealth;
+        horns = 3;
+        attack = (int) Math.round(Math.sqrt(mass)*horns);
+        herbivore = true;
+    }
+}
 class Pedro extends Animals {
     public Pedro() {
         mass = 30;
+        defense = 2;
+        maxHealth = mass;
+        health = maxHealth;
+        horns = 4;
+        attack = (int) Math.round(Math.sqrt(mass)*horns);
+        herbivore = false;
+    }
+}
+class Gruffalo extends Animals {
+    public Gruffalo() {
+        mass = 60;
         defense = 2;
         maxHealth = mass;
         health = maxHealth;
