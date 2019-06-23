037e8be944325c743a9e4ee5a437625532228018
Manja <ferdinand.ronngren@gmail.com>
2019-06-23 11:49:31 +0200

    fina(re) knappar

--- app/src/main/java/com/example/evolutionslek/StartScreen.java
+++ app/src/main/java/com/example/evolutionslek/StartScreen.java
@@ -1,7 +1,9 @@
 package com.example.evolutionslek;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+    }
+
+    private void animal1Chosen(View view){
+        Intent intent = new Intent(this, Ingame.class);
+        String animal = "Husmus";
+        intent.putExtra(animal, "Husmus" );
     }
 }
