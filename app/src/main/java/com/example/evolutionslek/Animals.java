3de860469363c70d6454559a8d12d5741819044a
Ruth <risberg.ruth@gmail.com>
2019-06-23 10:17:07 +0200

    updated mass, health, attack... parameters

--- app/src/main/java/com/example/evolutionslek/Animals.java
+++ app/src/main/java/com/example/evolutionslek/Animals.java
@@ -1,17 +1,25 @@
 package com.example.evolutionslek;
 
+import java.lang.Math();
+
 public class Animals {
-    int health; //0-100
-    int attack; //0-100
     int mass;   //1-100
+    int defense;//0-10 i början
+    int maxHealth; //1-100
+    int health; //0-100
+    int horns;  //0-10 i början
+    int attack; //0-100
     boolean herbivore;
 }
 
-class Pedros extends Animals {
-    public Pedros() {
-        health = 60;
-        attack = 60;
+class Pedro extends Animals {
+    public Pedro() {
         mass = 50;
+        defense = 2;
+        maxHealth = mass;
+        health = maxHealth;
+        horns = 4;
+        attack = (int) Math.round(Math.sqrt(mass)*horns);
         herbivore = false;
     }
 }
