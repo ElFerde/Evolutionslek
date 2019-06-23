318f4f3e76cb3e2a27ce971ef0089ab698cd6c20
Ruth <risberg.ruth@gmail.com>
2019-06-23 11:38:01 +0200

    added speed and claws

--- app/src/main/java/com/example/evolutionslek/Animals.java
+++ app/src/main/java/com/example/evolutionslek/Animals.java
@@ -4,21 +4,25 @@ import java.lang.Math;
 
 public class Animals {
     int mass;   //1-100
-    int defense;//0-10 i början
+    int horns;  //0-10
+    int speed;  //0-100
+    int defense;//0-10
     int maxHealth; //1-100
     int health; //0-100
-    int horns;  //0-10 i början
+    int claws;  //0-10
     int attack; //calcuated
     boolean herbivore;
 }
 
 class Husmus extends Animals {
     public Husmus() {
-        mass = 5;
+        mass = 15;
+        horns = 0;
+        speed = 80;
         defense = 1;
         maxHealth = mass;
         health = maxHealth;
-        horns = 1;
+        claws = 1;
         attack = (int) Math.round(Math.sqrt(mass)*horns);
         herbivore = true;
     }
@@ -26,10 +30,12 @@ class Husmus extends Animals {
 class Unicorn extends Animals {
     public Unicorn() {
         mass = 80;
+        horns = 1;
+        speed = 40;
         defense = 4;
         maxHealth = mass;
         health = maxHealth;
-        horns = 3;
+        claws = 0;
         attack = (int) Math.round(Math.sqrt(mass)*horns);
         herbivore = true;
     }
@@ -37,10 +43,12 @@ class Unicorn extends Animals {
 class Pedro extends Animals {
     public Pedro() {
         mass = 30;
+        horns = 0;
+        speed = 60;
         defense = 2;
         maxHealth = mass;
         health = maxHealth;
-        horns = 4;
+        claws = 7;
         attack = (int) Math.round(Math.sqrt(mass)*horns);
         herbivore = false;
     }
@@ -48,10 +56,12 @@ class Pedro extends Animals {
 class Gruffalo extends Animals {
     public Gruffalo() {
         mass = 60;
+        horns = 2;
+        speed = 15;
         defense = 2;
         maxHealth = mass;
         health = maxHealth;
-        horns = 4;
+        claws = 9;
         attack = (int) Math.round(Math.sqrt(mass)*horns);
         herbivore = false;
     }
