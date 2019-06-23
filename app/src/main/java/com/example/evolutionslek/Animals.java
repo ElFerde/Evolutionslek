7c1247d1921cb53190252ce196dfe8b08fd63602
Ruth <risberg.ruth@gmail.com>
2019-06-23 09:23:32 +0200

    added parameters health, attack, mass and herbivore

--- app/src/main/java/com/example/evolutionslek/Animals.java
+++ app/src/main/java/com/example/evolutionslek/Animals.java
@@ -1,9 +1,17 @@
 package com.example.evolutionslek;
 
 public class Animals {
-
+    int health; //0-100
+    int attack; //0-100
+    int mass;   //1-100
+    boolean herbivore;
 }
 
 class Pedros extends Animals {
-    
+    public Pedros() {
+        health = 60;
+        attack = 60;
+        mass = 50;
+        herbivore = false;
+    }
 }
