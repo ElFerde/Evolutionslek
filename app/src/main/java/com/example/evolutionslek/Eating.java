365251fca6dea169e8dc752f20f45569fca4b7d5
Ferdinand Rönngren <fro0204@student.skelleftea.se>
2019-08-06 11:20:47 +0200

    Nu kan man int äta orätt mat

--- app/src/main/java/com/example/evolutionslek/Eating.java
+++ app/src/main/java/com/example/evolutionslek/Eating.java
@@ -44,28 +44,36 @@ public class Eating extends AppCompatActivity implements ZXingScannerView.Result
         Toast.makeText(getApplicationContext(), data2[0], Toast.LENGTH_SHORT).show();
 
         if(djur.herbivore){
-            if(data2[1].equals(lastPlant)){
-                finish();
+            if(data2.length == 2){
+                if(data2[1].equals(lastPlant)){
+                    finish();
+                }
+                else {
+                    lastPlant = data2[1];
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("result", data2[0]);
+                    returnIntent.putExtra("plant", lastPlant);
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
+                }
             }
-            else{
-                lastPlant = data2[1];
-                Intent returnIntent = new Intent();
-                returnIntent.putExtra("result",data2[0]);
-                returnIntent.putExtra("plant",lastPlant);
-                setResult(Activity.RESULT_OK,returnIntent);
-                finish();
-            }
+            finish();
         }
         else{
+            boolean d = true;
+            if(data2.length < 8){
+                d = false;
+            }
             double a = Math.random();
             boolean success = true;
-            if(true)/*a/(1-a)<djur.speed/Double.parseDouble(data2[2]))*/{
+            if(d/*(a/(1-a)<djur.speed/Double.parseDouble(data2[2])*/){
                 while(Double.parseDouble(data2[4])>0) {
                     double b = Math.random();
                     if (b / 2 > djur.health / djur.maxHealth) {
                         success = false;
                         break;
-                    } else{
+                    }
+                    else{
                         data2[4] = Double.toString(Double.parseDouble(data2[4]) - djur.attack/Double.parseDouble(data2[3])*b*5 );
                         djur.health -= Double.parseDouble(data2[3])*b/djur.attack;
                         Toast.makeText(getApplicationContext(), "Hejhej", Toast.LENGTH_SHORT).show();
